bff3c3009bbc4e81c8723938275c88cf7cb1e25a
Steffen Uhlig <steffen@familie-uhlig.net>
2021-09-02 10:42:40 +0200

    Fix tests

--- spec/unit/wire_guard/admin/templates/client_spec.rb
+++ spec/unit/wire_guard/admin/templates/client_spec.rb
@@ -6,7 +6,11 @@ require 'wire_guard/admin/templates/client'
 require 'wire_guard/admin/client'
 
 describe WireGuard::Admin::Templates::Client do
-  subject(:template) { described_class.new(client, servers) }
+  subject(:template) { described_class.new(
+    client: client,
+    network: network,
+    servers: servers)
+  }
 
   context 'with no servers' do
     let(:client) do
@@ -17,7 +21,9 @@ describe WireGuard::Admin::Templates::Client do
         ip: IPAddr.new('1.2.3.4')
       )
     end
+
     let(:servers) { [] }
+    let(:network) { IPAddr.new('1.2.3.0/24') }
 
     it 'has an Interface section' do
       expect(template.render).to include('[Interface]')
